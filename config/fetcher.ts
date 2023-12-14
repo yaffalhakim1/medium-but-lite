64e01546e19d47c7c39f4bad2eaaf60715ca1226
Muhammad Yafi Al Hakim <yafi.alhakim@shopee.com>
2023-12-14 09:14:22 +0700

    feat(user): handling error for fecther improved

--- config/fetcher.ts
+++ config/fetcher.ts
@@ -1,11 +1,31 @@
 import axios from "axios";
 
 export const fetcher = async (url: string) => {
-  const token = localStorage.getItem("token");
-  const res = await axios.get(url, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
-  return res.data;
+  try {
+    const resp = await axios.get(url);
+
+    if (resp.status !== 200) {
+      throw new Error(`Request failed with status code ${resp.status}`);
+    }
+
+    if (!resp.data) {
+      throw new Error("Data not found");
+    }
+    return resp.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      if (error.response) {
+        console.error(
+          `Server responded with status code ${error.response.status}`
+        );
+        console.error(error.response.data);
+      } else if (error.request) {
+        console.error("No response received");
+      } else {
+        console.error(`Request failed with message ${error.message}`);
+      }
+    }
+  }
+
+  return null;
 };
