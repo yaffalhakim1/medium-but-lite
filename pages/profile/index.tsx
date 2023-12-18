933cf52cbde4ae552e45fe88f4b2d0b9acaf42cc
Muhammad Yafi Al Hakim <yafi.alhakim@shopee.com>
2023-12-18 12:01:06 +0700

    feat(user):Displays personal information, including name, username, email, phone number, and address. (UI not good yet)

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,7 +1,33 @@
+import { useUser } from "@/lib/useUser";
 import React from "react";
+import Cookie from "js-cookie";
+import { formatExpirationDate } from "@/lib/utils/user-subs";
 
 const ProfilePage = () => {
-  return <div>ProfilePage</div>;
+  // including  phone number, and address.
+
+  const profileId = Cookie.get("user_id");
+  const { user } = useUser(Number(profileId));
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center">
+        <h2>{user?.name}</h2>
+        <p>{user?.email}</p>
+        <p>
+          {user?.isPremiumUser ? (
+            <div className="badge badge-success px-2 py-3 text-white text-sm">
+              Active
+            </div>
+          ) : (
+            <div className="badge badge-error px-2 py-3 text-white text-sm">
+              Not Active
+            </div>
+          )}
+        </p>
+        <p>{formatExpirationDate(user?.expiredDate)}</p>
+      </div>
+    </>
+  );
 };
 
 export default ProfilePage;
